cad8f9013f9c170fc10978172a6d2a985cb1d4ed
Breyanna Vass <breyannavass@gmail.com>
2019-10-16 19:43:34 -0400

    importing grid and youtube api file then setting up for components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Grid } from "material-ui";
+
+import YouTube from "./api/youtube";
 
 import "./App.css";
 
@@ -6,7 +9,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>YouTube Clone App</h1>
+        <Grid justify="center" container spacing={16}>
+          <Grid item xs={12}>
+            <Grid container spacing={16}>
+              <Grid item xs={12}>
+                {/* Search Bar */}
+              </Grid>
+              <Grid item xs={8}>
+                {/* Video Details */}
+              </Grid>
+              <Grid item xs={4}>
+                {/* Video list */}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     );
   }
