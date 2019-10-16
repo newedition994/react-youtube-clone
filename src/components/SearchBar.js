57d9909b54e17bf41bcba644fc0bac61300e5d02
Breyanna Vass <breyannavass@gmail.com>
2019-10-16 19:49:39 -0400

    making the searchbar component a class based component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    searchTerm: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>This is a SearchBar Component</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
