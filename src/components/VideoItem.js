ee26eefc2a26c4474ea56562f654335c2d93244c
Breyanna Vass <breyannavass@gmail.com>
2019-10-16 21:44:01 -0400

    getting the thumbnails of the other video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,18 @@ import React from "react";
 import { Grid, Paper, Typography } from "@material-ui/core";
 
 const VideoItem = ({ video }) => {
-  return <h1>Video Item</h1>;
+  return (
+    <Grid item xs={12}>
+      <Paper style={{ display: "flex", alignItems: "center" }}>
+        <img
+          style={{ marginRight: "20px" }}
+          alt="thumbnail"
+          src={video.snippet.thumbnails.medium.url}
+        />
+        <Typography variant="subtitle1">{video.snippet.title}</Typography>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default VideoItem;
